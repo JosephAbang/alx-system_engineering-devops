dac420d5bb81c7db14ca82bd790dbd00a9fe0c9a
root <root@DESKTOP-KML44A3.localdomain>
2023-11-25 04:07:23 +0100

    Install package: flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # Install flask using puppet
 
+package { 'python3-pip':
+    ensure => 'installed',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+}
+
+# Install a compatible version of Werkzeug
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    require  => Package['python3-pip'],
 }
