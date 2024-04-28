b241d07a4edfb747582055447a398533ffb89fc1
JosephAbang <abangjosef@gmail.com>
2024-04-28 05:18:32 +0100

    Fixed json.dump statement to correctly export to-do list information of all employees to JSON format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,12 +10,17 @@ import requests
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     _users = requests.get(url + 'users').json()
+    all_employees_data = {}
+
+    for user in _users:
+        user_id = user.get('id')
+        user_todos = requests.get(url + 'todos',
+                                        params={'userId': user_id}).json()
+        user_tasks = [{'task': todo.get('title'),
+                    'completed': todo.get('completed'),
+                    'username': user.get('username')}
+                for todo in user_todos]
+        all_employees_data[user_id] = user_tasks
+
     with open('todo_all_employees.json', 'w+') as json_f:
-        json.dump({
-            j.get('id'): [{
-                'task': i.get('title'),
-                'completed': i.get('completed'),
-                'username': j.get('username')
-            } for i in requests.get(url + 'todos',
-                                    params={'userId': j.get('id')}).json()]
-            for j in _users}, json_f)
+        json.dump(all_employees_data, json_f)
