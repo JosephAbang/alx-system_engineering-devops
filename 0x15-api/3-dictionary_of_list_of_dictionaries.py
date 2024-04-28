76ea98bc1cadf69732e1da967df0a0a45a9a9113
JosephAbang <abangjosef@gmail.com>
2024-04-28 05:38:04 +0100

    Fix code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,6 +8,9 @@ import requests
 
 
 if __name__ == '__main__':
+    import json
+    import requests
+    import sys
 
     users = requests.get("https://jsonplaceholder.typicode.com/users").json()
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
@@ -18,8 +21,8 @@ if __name__ == '__main__':
         for task in todos:
             if task.get('userId') == user.get('id'):
                 task_dict = {"username": user.get('username'),
-                            "task": task.get('title'),
-                            "completed": task.get('completed')}
+                                "task": task.get('title'),
+                                "completed": task.get('completed')}
                 task_list.append(task_dict)
         all_todo[user.get('id')] = task_list
 
