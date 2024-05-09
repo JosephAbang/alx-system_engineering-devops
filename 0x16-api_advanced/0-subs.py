90c9d881cd4d8f5244a18af99bfd7490894cef59
JosephAbang <abangjosef@gmail.com>
2024-05-09 01:56:39 +0100

    Fix pycodestyle error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,10 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns:
-        int: The number of subscribers of the subreddit. 
+        int: The number of subscribers of the subreddit.
         Returns 0 if the subreddit is invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
