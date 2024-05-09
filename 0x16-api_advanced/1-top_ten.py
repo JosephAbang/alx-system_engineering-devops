ff9c221aa7ba5dcc832ccf735f50f36a2b89d01c
JosephAbang <abangjosef@gmail.com>
2024-05-09 02:24:26 +0100

    Print the titles of the first 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the 10 hottest posts on a given subreddit"""
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        Print(None)
